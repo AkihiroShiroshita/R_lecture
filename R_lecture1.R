cc464841bc3aafd5e31c47265ea681684c7d765b
Your Naame <you@example.com>
2020-07-14 08:24:31 +0000

    advanced cleaning

--- R_lecture1.R
+++ R_lecture1.R
@@ -6,7 +6,9 @@
 install.packages("usethis") #to creat readme file
 install.packages("rmarkdown")
 usethis::use_readme_rmd()
-###Data cleaning############
+###################
+###Data cleaning###
+###################
 getwd() #setwd("~/project")
 library(readxl)
 od1 <- read_excel("original_data.xlsx",
@@ -18,6 +20,7 @@ install.packages("lubridate")
 library(tidyverse)
 library(lubridate)
 #Datacleaning: sheet1
+colnames(od1)
 od1 <- od1 %>% 
   rename(id = 患者番号,
          age = 入院時年齢,
@@ -26,7 +29,7 @@ od1 <- od1 %>%
          prognosis = 退院時転帰)
 od1 <- od1 %>% 
   mutate(adl = apply(od1[,16:25], 1, sum),
-         hospitalterm = interval(original_data$退院年月日, original_data$入院年月日)) 
+         hospitalterm = interval(od1$退院年月日, od1$入院年月日)) 
 od1 <- od1 %>% 
   select(id, age, steroid, intubation, prognosis, adl) 
 #Datacleaning: sheet2
@@ -44,4 +47,32 @@ od2_sub <- str_split(od2$bp, "/", simplify=T) %>%
          dbp= V2)
 od2_2_sub <- bind_cols(od2, od2_sub) 
 #Connecting od1 and od3
-od13 <- inner_join(od1, od2_2_sub, by = "id")  
+od12 <- inner_join(od1, od2_2_sub, by = "id")
+#Saving data
+save(od1, od2_2_sub, od12, file = "after_cleaning.rda")
+od12 %>% write_csv(path="cleaned_original_data.csv")
+#Reload RDA files
+load("/cloud/project/after_cleaning.rda")
+#Connecting data
+
+############################
+###Advanced data cleaning###
+############################
+rp1 <- read_excel("original_data_relapse.xlsx", 
+                  sheet = "入契肺炎")
+dim(rp1)
+rp1 <- rp1 %>% 
+  rename(id = 患者番号,
+         age = 入院時年齢,
+         sex = 性別,
+         start = 入院年月日,
+         end = 退院年月日)
+#Check the number of relapse patients
+rp1_grouped <- rp1 %>% 
+  group_by(id) %>% 
+  filter(n() >= 2)
+length(unique(rp1_grouped$id)) #Not use "rp1_grouped[id]".
+#rp1_grouped %>% ungroup()
+rp2 <- rp1_grouped %>% 
+  mutate(tag = start - lag(end)) %>% 
+  filter(tag <= 365)
